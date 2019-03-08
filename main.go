1be130b7d78904e86b8b764409b3e80a0b561b9b
Alessio Treglia <alessio@tendermint.com>
2019-03-08 02:38:21 +0000

    Initial release

---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	progName string
+	// sections name-title map
+	sections = map[string]string{
+		"breaking":     "Breaking Changes",
+		"features":     "New features",
+		"improvements": "Improvements",
+		"bugfixes":     "Bugfixes",
+	}
+	// stanzas name-title map
+	stanzas = map[string]string{
+		"gaia":       "Gaia",
+		"gaiacli":    "Gaia CLI",
+		"gaiarest":   "Gaia REST API",
+		"sdk":        "SDK",
+		"tendermint": "Tendermint",
+	}
+)
+
+func init() {
+	progName = filepath.Base(os.Args[0])
+	flag.Usage = printUsage
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix(fmt.Sprintf("%s: ", filepath.Base(progName)))
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		errInsufficientArgs()
+	}
+
+	changesDir := flag.Arg(0)
+	cmd := flag.Arg(1)
+	switch cmd {
+	case "init":
+		initChangesDir(changesDir)
+	case "generate":
+		generateChangelog(changesDir)
+	}
+}
+
+func initChangesDir(changesDir string) {
+	if _, err := os.Stat(changesDir); os.IsNotExist(err) {
+		if err := os.Mkdir(changesDir, 0755); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatalf("directory %q already exists", changesDir)
+	}
+
+	for _, section := range sections {
+		for _, stanza := range stanzas {
+			path := filepath.Join(changesDir, section, stanza)
+			if err := os.MkdirAll(path, 0755); err != nil {
+				log.Fatal(err)
+			}
+			// create stamp file to allow git commit of the dir structure
+			os.Create(filepath.Join(path, ".stamp"))
+		}
+	}
+}
+
+func generateChangelog(changesDir string) {
+	fmt.Printf("# UNRELEASED\n\n")
+	for sectionDir, sectionTitle := range sections {
+
+		fmt.Printf("## %s\n\n", sectionTitle)
+		for stanzaDir, stanzaTitle := range stanzas {
+			fmt.Printf("### %s\n\n", stanzaTitle)
+			path := filepath.Join(changesDir, sectionDir, stanzaDir)
+			files, err := ioutil.ReadDir(path)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, f := range files {
+				if f.Name()[0] == '.' {
+					continue
+				}
+				if err := indentAndPrintFile(filepath.Join(path, f.Name())); err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			fmt.Println()
+		}
+	}
+}
+
+func indentAndPrintFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	firstLine := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(strings.TrimSpace(line)) == 0 {
+			continue
+		}
+
+		if firstLine {
+			fmt.Printf("* %s\n", scanner.Text())
+			firstLine = false
+			continue
+		}
+
+		fmt.Printf("  %s\n", scanner.Text())
+	}
+
+	return scanner.Err()
+}
+
+func printUsage() {
+	usageText := fmt.Sprintf(`usage: %s DIRECTORY COMMAND
+
+Commands:
+    init
+    generate VERSION              Generate a changelog in
+                                  Markdown format and print it
+                                  to stdout.
+`, progName)
+	fmt.Fprintf(os.Stderr, "%s\n", usageText)
+	//flag.PrintDefaults()
+}
+
+func errInsufficientArgs() {
+	log.Fatalf("insufficient arguments\nTry '%s -help' for more information.", progName)
+}
+
+func errTooManyArgs() {
+	log.Fatalf("too many arguments\nTry '%s -help' for more information.", progName)
+}
+
+func unknownCommand(cmd string) {
+	log.Fatalf("unknown command -- '%s'\nTry '%s -help' for more information.", cmd, progName)
+}
